bbe7d89a76d58ebe526024cc450f89d49e16c3d5
6teendollars <91662992+6teendollars@users.noreply.github.com>
2023-11-06 12:24:28 +0100

    comit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import NavBar from "./components/Navbar.jsx";
+import Home from "./pages/Home.jsx";
+import { AuthContextProvider } from "./context/AuthContext.js";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import Account from "./pages/Account.jsx";
+
+function App() {
+  return (
+    <>
+      <AuthContextProvider>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/account" element={<Account />} />
+        </Routes>
+      </AuthContextProvider>
+    </>
+  );
+}
+
+export default App;
